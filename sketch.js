b6d1c0f3e593e12c25ef758e5602a05ed8d31572
Dylan Neve <dylanneve1@gmail.com>
2019-10-09 20:12:08 +0100

    Add a little template and disable scrolling

--- sketch.js
+++ sketch.js
@@ -1,9 +1,31 @@
-var drops = [];
+var w = window.innerWidth;
+var h = window.innerHeight;
 
 function setup() {
-  createCanvas(640, 360);
+  createCanvas(window.screen.width, window.screen.height);
 }
 
 function draw() {
-  background(230, 230, 250);
+  basics();
+  texts();
+  keepWindowSizeUpToDate();
+}
+
+function basics() {
+  background(100);
+  rectMode(CORNER);
+  fill(200);
+  rect(0, 0, w, h/4);
+}
+
+function texts() {
+  fill(0);
+  textAlign(CENTER);
+  textSize(40);
+  text("Pairorg Help Bot", w/2, h/7);
+}
+
+function keepWindowSizeUpToDate() {
+  w = window.innerWidth;
+  h = window.innerHeight;
 }
